f2ee9aaae542825faff7b4f49b4b8cb357ce0feb
Kanchan Rajput <rajputkanchan601@gmail.com>
2026-02-11 17:57:51 +0530

    message

--- script.js
+++ script.js
@@ -40,9 +40,23 @@ nav.addEventListener("mouseleave",function(){
 
  rightElems.forEach(function(elem){
     elem.addEventListener("mouseenter",function(){
-        console.log(elem.childNodes[1])
+        
+        gsap.to(elem.childNodes[3],{
+            opacity:1,
+            scale:1
+        })
     })
      elem.addEventListener("mouseleave",function(){
-        console.log("hello")
+        gsap.to(elem.childNodes[3],{
+            opacity:0,
+            scale:0
+        }) 
     })
- })
+elem.addEventListener("mousemove",function(dets){
+    gsap.to(elem.childNodes[3],{
+        x:dets.x - elem.getBoundingClientRect().x-50,
+        y:dets.y - elem.getBoundingClientRect().y-190
+    })
+    })
+})
+
