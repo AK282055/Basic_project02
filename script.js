75edf043c9a6b1b51a1a6591cf9d4e5d0abed7a8
Kanchan Rajput <rajputkanchan601@gmail.com>
2026-02-14 18:28:08 +0530

    message

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ elem.addEventListener("mousemove",function(dets){
     })
 })
 
-var page3Center = document.querySelector(".page-center")
+var page3Center = document.querySelector(".page3-center")
 var video = document.querySelector("#page3 video")
 
 page3Center.addEventListener("click",function(){
@@ -69,5 +69,13 @@ page3Center.addEventListener("click",function(){
         transform:"scaleX(1) scaleY(1)",
         opacity:1,
         borderRadius:0
+    }
+})
+video.addEventListener("click",function(){
+    video.pause()
+    gsap.to(video),{
+        transform:"scaleX(0.7) scaleY(0)",
+        opacity:0,
+        borderRadius:"30px"
     }
 })
